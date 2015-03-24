909a172abe751644f12cb295af91e9fc671f4313
Andrew <astalke@purdue.edu>
2015-03-24 09:20:52 -0400

    Update alphaNumeric.c

--- CNIT-315/Lab4/alphaNumeric.c
+++ CNIT-315/Lab4/alphaNumeric.c
@@ -1,10 +1,10 @@
 /*  Andrew Stalker */
-
 /* Lab 4 tester to determine if a character is alpha-numeric */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h> //for standard functions isalpha() and isdigit()
+#include <time.h>
 
 int main() {
 	/*Variables*/
@@ -15,18 +15,33 @@ int main() {
 	printf("\nEnter anything:");
 	scanf("%c", &userInput);
 
-	if(isalpha(userInput)) 
-	{
-		printf("\n--It's a letter!--\n");
-	}
-	else if (isdigit(userInput))
-	{
-		printf("\n--It's a number!--\n");
-	}
-	else
-	{
-		printf("\n--It's something else?--\n");
-	}
+	/*Function to Test*/
+	alphaNumeric(char c);
+	
+	char randChar = ‘ ‘;
+	int counter = 0;
+	int randNum = 0;
 
+     // Provide seed for the random number     
+	// generator.
+      srand(time(NULL));
+      for (counter = 0; counter < 10; counter++)
+      {
+          // First, pick a number between 0 and
+          // 25.
+          randNum = 26 * (rand() / (RAND_MAX +
+              1.0));
+
+          // The ASCII code for ‘a’ is 97, so
+          // add 97 to the random number to get
+          // a lower case letter.
+          randNum = randNum + 97;
+
+          // Type cast to character to convert
+          // it to a char type.
+          randChar = (char) randNum;
+
+         printf (“Random char: %c\n“, randChar);
+	
 return 0;
 }
